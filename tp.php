61b79dc60f36db9c4d838ac97c413733531bfdbe
ALAN <alaaneltom@gmail.com>
2021-11-19 17:13:58 -0300

    Create tp.php

---
+++ tp.php
@@ -0,0 +1,12 @@
+<?php
+
+$nombre = "Alan.";
+$apellido = "Alzogaray.";
+$edad = "23 años.";
+$hobbie = "Hacer deporte - futbol, gimnasio.";
+$editor = "Visual Studio Code.";
+$so = "Windows.";
+
+echo "$nombre <br><br> $apellido<br><br> $edad<br><br> $hobbie<br><br> $editor<br><br> $so<br><br>";
+
+?>
